24cd95887688182e9117d0a16b837164dd250828
luisgthub <luiamaral2@gmail.com>
2024-07-23 18:57:52 -0300

    Created the ApplicationDbContext and respective configurations

---
+++ FastFood.Repository/DbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using FastFood.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastFood.Repository
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public DbInitializer(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initializer()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            if (_context.Roles.Any(x => x.Name == "Admin")) return;
+            _roleManager.CreateAsync(new IdentityRole("Manager")).GetAwaiter();
+            _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter();
+            _roleManager.CreateAsync(new IdentityRole("Customer")).GetAwaiter();
+
+            var user = new ApplicationUser()
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Name = "Admin",
+                City = "xxx",
+                Address = "zzz",
+                PostalCode = "3222",
+
+            };
+            _userManager.CreateAsync(user, "Admin@123").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(user, "Admin");
+        }
+    }
+}
